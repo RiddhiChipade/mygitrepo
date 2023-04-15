3ace93a8075453f40d6350ce3535955dd374040c
riddhi1602 <riddhi.chipade@cumminscollege.in>
2023-04-15 11:46:21 +0530

    Created A.java

--- A.java
+++ A.java
@@ -2,6 +2,6 @@
 
 class A {
     public static void main(String[] args) {
-        System.out.println("Hello!!!"); 
+        System.out.println("Have a wonderful Day!!! from A"); 
     }
 }
---
+++ B.java
@@ -0,0 +1,5 @@
+class A {
+    public static void main(String[] args) {
+        System.out.println("Have a wonderful Day!!! from B"); 
+    }
+}
